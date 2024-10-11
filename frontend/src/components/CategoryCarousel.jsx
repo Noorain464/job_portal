fc8d5147eecb59ded31e5c56260531c1f3923bc8
Syeda Noorain <syeda23bcs10221@ms.sst.scaler.com>
2024-10-12 00:49:30 +0530

    Category carousel

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import { Carousel , CarouselContent, CarouselItem,CarouselPrevious,CarouselNext} from './ui/carousel'
+import { Button } from './ui/button'
+const category = [
+    "Frontend Developer",
+    "Backend Developer",
+    "Data Scientist",
+    "Graphic Designer",
+    "Fullstack Developer",
+]
 function CategoryCarousel() {
   return (
-    <div>CategoryCarousel</div>
+    <div>
+        <Carousel className="w-full max-w-xl mx-auto my-20">
+            <CarouselContent>
+                    {
+                        category.map((item, index) => (
+                           <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                                <Button variant="outline" className="rounded-full">{item}</Button>
+                           </CarouselItem>
+                        ))
+                    }
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    </div>
   )
 }
 
