671f6bfb6b74fa8501665ee916ac23f875fd1352
Syeda Noorain <syeda23bcs10221@ms.sst.scaler.com>
2024-10-21 18:06:52 +0530

    changes made

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,79 +1,66 @@
-import React from "react";
-import Navbar from "./shared/Navbar";
-import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
-import { Button } from "./ui/button";
-import ApplicationTable from "./ApplicationTable";
-import { Pen } from "lucide-react";
-import { Mail } from "lucide-react";
-import { Contact } from "lucide-react";
-import { Badge } from "./ui/badge";
-import { Label } from "@radix-ui/react-label";
-import UpdateProfileDialog from "./UpdateProfileDialog";
-import { useSelector } from "react-redux";
+import React, { useState } from 'react'
+import Navbar from './shared/Navbar'
+import { Avatar, AvatarImage } from './ui/avatar'
+import { Button } from './ui/button'
+import { Contact, Mail, Pen } from 'lucide-react'
+import { Badge } from './ui/badge'
+import { Label } from './ui/label'
+import UpdateProfileDialog from './UpdateProfileDialog'
+import { useSelector } from 'react-redux'
 
-const isPresent = true;
+const isResume = true;
 
-function Profile() {
-    const [open, setOpen] = React.useState(false);
-    const {user}= useSelector(store=>store.auth);
+const Profile = () => {
+    const [open, setOpen] = useState(false);
+    const {user} = useSelector(store=>store.auth);
+
     return (
         <div>
             <Navbar />
-            <div className="max-w-4xl mx-auto bg-white border-gray-200 rounded-2xl my-5 p-8">
-                <div className="flex justify-between">
-                    <div className="flex items-center gap-4">
+            <div className='max-w-4xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8'>
+                <div className='flex justify-between'>
+                    <div className='flex items-center gap-4'>
                         <Avatar className="h-24 w-24">
-                            <AvatarImage src={user?.profile?.profilePhoto} />
+                            <AvatarImage src={user?.profile?.profilePhoto} alt="profile" />
                         </Avatar>
                         <div>
-                            <h1 className="font-medium text-xl">{user?.fullname}</h1>
+                            <h1 className='font-medium text-xl'>{user?.fullname}</h1>
                             <p>{user?.profile?.bio}</p>
                         </div>
-                        <Button onClick={() => setOpen(true)} className="text-right">
-                            <Pen />
-                        </Button>
                     </div>
+                    <Button onClick={() => setOpen(true)} className="text-right" variant="outline"><Pen /></Button>
                 </div>
-                <div className="my-5">
-                    <div className="flex items-center gap-3 my-2">
+                <div className='my-5'>
+                    <div className='flex items-center gap-3 my-2'>
                         <Mail />
                         <span>{user?.email}</span>
                     </div>
-                    <div className="flex items-center gap-3 my-2">
+                    <div className='flex items-center gap-3 my-2'>
                         <Contact />
                         <span>{user?.phoneNumber}</span>
                     </div>
                 </div>
-                <div className="my-5">
+                <div className='my-5'>
                     <h1>Skills</h1>
-                    <div className="flex items-center gap-1">
-
+                    <div className='flex items-center gap-1'>
                         {
-                            user?.profile?.skills.length === 0 ? <span>No Skills Found</span> : (
-
-                                user?.profile?.skills.map((item, index) => (
-                                    <Badge key={index} className="bg-black text-white hover:bg-black text-white">{item}</Badge>
-                                ))
-                            )
+                            user?.profile?.skills.length !== 0 ? user?.profile?.skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
                         }
                     </div>
                 </div>
-                <div className="grid w-full max-w-sm items-center gap-1.5">
-                    <Label className="text-medium font-bold">Resume</Label>
+                <div className='grid w-full max-w-sm items-center gap-1.5'>
+                    <Label className="text-md font-bold">Resume</Label>
                     {
-                        isPresent ? (
-                            <a target="blank" href={user?.profile?.resume} className="text-blue-500 w-full hover:underline">My Resume</a>
-                        ) : <span>No Resume</span>
+                        isResume ? <a target='blank' href={user?.profile?.resume} className='text-blue-500 w-full hover:underline cursor-pointer'>{user?.profile?.resumeOriginalName}</a> : <span>NA</span>
                     }
                 </div>
             </div>
-            <div className="max-w-4xl mx-auto bg-white rounded-2xl">
-                <h1 className="font-bold text-lg my-5">Applied Jobs</h1>
-                <ApplicationTable />
+            <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
+                <h1 className='font-bold text-lg my-5'>Applied Jobs</h1>
             </div>
-            <UpdateProfileDialog open={open} setOpen={setOpen} />
+            <UpdateProfileDialog open={open} setOpen={setOpen}/>
         </div>
-    );
+    )
 }
 
-export default Profile;
+export default Profile
