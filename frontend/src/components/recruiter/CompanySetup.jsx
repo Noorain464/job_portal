543aa2c3e28344b4e2cffa890817ae811428352c
Syeda Noorain <syeda23bcs10221@ms.sst.scaler.com>
2024-12-12 17:40:51 +0530

    company setup done

--- frontend/src/components/recruiter/CompanySetup.jsx
+++ frontend/src/components/recruiter/CompanySetup.jsx
@@ -4,6 +4,10 @@ import { ArrowLeft } from 'lucide-react'
 import { Button } from '../ui/button'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
+import axios from 'axios'
+import { COMPANY_API_END_POINT } from '@/utils/constant'
+import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'sonner'
 
 const CompanySetup = () => {
     const [input, setInput] = useState({
@@ -13,7 +17,9 @@ const CompanySetup = () => {
         location: "",
         file: null
     })
-
+    const [loading, setLoading] = useState(false);
+    const params = useParams();
+    const navigate = useNavigate();
     const changeEventHandler = (e) => {
         setInput({
             ...input,
@@ -21,16 +27,41 @@ const CompanySetup = () => {
         })
     }
     const changeFileHandler = (e) => {
-        setInput({
-            ...input,
-            file: e.target.files?.[0]
-        })
+        const file = e.target.files?.[0];
+        setInput({...input,file})
+    }
+    const submitHandler = async(e) => {
+        e.preventDefault();
+        const formdata = new FormData();
+        formdata.append("name", input.name);
+        formdata.append("description", input.description);
+        formdata.append("website", input.website);
+        formdata.append("location", input.location);
+
+        if(input.file){
+            formdata.append("file", input.file);
+        }
+        try {
+            const res = await axios.put(`${COMPANY_API_END_POINT}/update/${params.id}`, formdata,{
+                headers: {
+                    'Content-Type' : 'multipart/form-data'
+                },
+                withCredentials:true
+            })
+            if(res.data.success){
+                toast.success(res.data.message);
+                navigate("/admin/companies");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error?.response?.data?.message);
+        }
     }
     return (
         <div>
             <Navbar />
             <div className='max-w-xl mx-auto my-10'>
-                <form action="">
+                <form onSubmit={submitHandler}>
                     <div className='flex items-center gap-5 p-8'>
                         <Button variant="outline" className="flex items-center gap-2 text-gray-500 font semibold">
                             <ArrowLeft />
@@ -52,31 +83,39 @@ const CompanySetup = () => {
                             <Label>Description</Label>
                             <Input
                                 type="text"
-                                name="name"
+                                name="description"
                                 value={input.description}
                                 onChange={changeEventHandler}
                             />
                         </div>
                         <div>
-                            <Label>Company Name</Label>
+                            <Label>Website</Label>
                             <Input
                                 type="text"
-                                name="name"
-                                value={input.name}
+                                name="website"
+                                value={input.website}
                                 onChange={changeEventHandler}
                             />
                         </div>
                         <div>
-                            <Label>Company Name</Label>
+                            <Label>Location</Label>
                             <Input
                                 type="text"
-                                name="name"
-                                value={input.name}
+                                name="location"
+                                value={input.location}
                                 onChange={changeEventHandler}
                             />
                         </div>
+                        <div>
+                            <Label>Logo</Label>
+                            <Input
+                                type="file"
+                                accept="image/*"
+                                onChange={changeFileHandler}
+                            />
+                        </div>
                     </div>
-
+                    <Button type="submit" className="w-full mt-8 bg-[#6A38C2] text-white">Update</Button>
                 </form>
             </div>
         </div>
